9fcb258a0509012f4f3202fb16bd2607685b9c60
Alex Sova <alex.sova.jam@gmail.com>
2024-01-20 16:27:14 -0300

    chore: community section change

--- src/2x/md/community.md.js
+++ src/2x/md/community.md.js
@@ -2,12 +2,11 @@ export default /*md*/ `
 ## Symbiote.js in social networks
 
 - [GitHub Discussions](https://github.com/symbiotejs/symbiote.js/discussions)
+- [DEV.to](https://dev.to/symbiotejs)
+- [Stack Overflow](https://stackoverflow.com/questions/tagged/symbiote+or+symbiote.js)
 - [Reddit](https://www.reddit.com/r/symbiotejs/)
-- [Telegram](https://t.me/symbiotejs)
-- [Twitter](https://twitter.com/symbiotejs)
 - [YouTube](https://www.youtube.com/channel/UCHtF8Y8wM2-LaFc6tx0ypjA)
 - [Facebook](https://www.facebook.com/groups/857783385473595)
-- [DEV.to](https://dev.to/symbiotejs)
 
 > Join us to get latest updates and hottest news!
 `;
