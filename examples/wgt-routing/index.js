cb7c9f0c896f98b33fc040b9866e87662ccec6df
Alex Sova <alex.sova.jam@gmail.com>
2024-01-21 18:47:28 -0300

    feat: widget routing example added

---
+++ examples/wgt-routing/index.js
@@ -0,0 +1,71 @@
+import Symbiote, { html, PubSub } from '@symbiotejs/symbiote';
+
+let routes = [
+  {
+    route: 'home',
+    title: 'Home',
+    options: {
+      timestamp: Date.now(),
+    },
+  },
+  {
+    route: 'user',
+    title: 'User',
+    options: {
+      timestamp: Date.now(),
+    },
+  },
+  {
+    route: 'settings',
+    title: 'Settings',
+    options: {
+      timestamp: Date.now(),
+    },
+  },
+];
+
+const router = PubSub.registerCtx(routes[0], 'R');
+
+class AppShell extends Symbiote {
+  init$ = {
+    routes: structuredClone(routes),
+
+    '+optionsJson': () => {
+      return JSON.stringify(this.$['R/options'], undefined, 2);
+    },
+    '+sectionHtml': () => {
+      let sec = this.$['R/route'];
+      return `<${sec}-section></${sec}-section>`;
+    },
+
+    onNav: (e) => {
+      let route = e.target.getAttribute('route');
+      if (route) {
+        let routeDescriptor = routes.find((desc) => {
+          return desc.route === route;
+        });
+        if (routeDescriptor) {
+          routeDescriptor.options.timestamp = Date.now();
+          router.multiPub(routeDescriptor);
+          this.notify('+optionsJson');
+          this.notify('+sectionHtml');
+        }
+      }
+    },
+  }
+}
+
+AppShell.template = html`
+  <h1>Section title: {{R/title}}</h1>
+  <h2>Current route: {{R/route}}</h2>
+  <label>Navigation panel:</label>
+  <nav ${{itemize: 'routes', onclick: 'onNav'}}>
+    <button ${{'@route': 'route'}}>{{title}}</button>
+  </nav>
+  <label>Route options:</label>
+  <code>{{+optionsJson}}</code>
+  <label>Current section wrapper element:</label>
+  <div class="viewport" ${{innerHTML: '+sectionHtml'}}>Current section tag goes here...</div>
+`;
+
+AppShell.reg('app-shell');
--- src/2x/home/sections.js
+++ src/2x/home/sections.js
@@ -54,6 +54,7 @@ export default /*html*/ `
       <li><a href="./2x/playground/ssr-hydration/" target="_blank">SSR + hydration</a></li>
       <li><a href="./2x/playground/icons/" target="_blank">Icons</a></li>
       <li><a href="./2x/playground/icons-2/" target="_blank">Icons 2</a></li>
+      <li><a href="./2x/playground/wgt-routing/" target="_blank">Widget routing</a></li>
       <!-- <li><a href="./2x/playground/ssr-css/" target="_blank">SSR + CSS Data</a></li> -->
     </ul>
   </section-inner>
