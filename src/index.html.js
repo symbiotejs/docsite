b5b4a18928f9316ccec635ea9ae37fdce28281cb
foxeyes <foxrandom.com@gmail.com>
2025-12-18 18:57:01 -0300

    chore: Deprecate symbiotejs.org, redirecting users to rnd-pro.com/symbiote and simplifying the main page.

--- src/index.html.js
+++ src/index.html.js
@@ -10,59 +10,19 @@ ${head(undefined, '<script src="./2x/js/index.js" type="module"></script>')}
     <img src="./svg/logo/index.svg" alt="Symbiote.js">
   </card-el>
 
-  <a href="#new">
-    <card-el triple>
+  <a href="https://rnd-pro.com/symbiote/">
+    <card-el triple clr-1>
       <div>
-        <h2>Symbiote.js 2.3.x is out now!</h2>
-        <h3>What's new?</h3>
+        <h2>⚠️ This site is deprecated and no longer updated.</h2>
+        <h3>Please go to our new site instead</h3>
+        <h3>rnd-pro.com/symbiote</h3>
       </div>
     </card-el>
   </a>
 
-  <a href="./2x/docs/Get_started/">
-    <card-el clr-2>Docs</card-el>
-  </a>
-
-  <a href="#philosophy">
-    <card-el clr-5>Philosophy</card-el>
-  </a>
-  
-  <a href="#live">
-    <card-el double clr-2>Live example</card-el>
-  </a>
-  
-  <a href="#playground">
-    <card-el clr-4>Playground</card-el>
-  </a>
-
-  <a href="#biome">
-    <card-el>Biome</card-el>
-  </a>
-
-  <a href="https://chat.openai.com/g/g-7GbkxAPqG-symbiote-ai"  target="_blank">
-    <card-el double clr-5>Symbiote AI  →</card-el>
-  </a>
-
-  <a href="#community">
-    <card-el>Community</card-el>
-  </a>
-
-  <a href="#sponsorship">
-    <card-el double clr-3>Sponsorship</card-el>
-  </a>
-
-  <a href="https://rnd-pro.com/" target="_blank">
-    <card-el triple>Request solution</card-el>
-  </a>
-
   <a href="./1x/" target="_blank">
-    <card-el>Go to 1.x →</card-el>
+    <card-el clr-2 double>1.x version docs →</card-el>
   </a>
 
-  <a href="https://github.com/symbiotejs/symbiote.js" target="_blank">
-    <card-el clr-2>GitHub →</card-el>
-  </a>
-
-  ${sections}
 </body>
 </html>`;
